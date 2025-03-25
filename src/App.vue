87206b2eeb282be0e709ebba79ace1ab96a50432
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2025-03-25 08:50:11 -0300

    Atualizacao

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ function rev(){
 }
 </script>
 <template>
-  <h1>contador</h1>
+  <h1>contador 2info1</h1>
   <h2>{{ cont }}</h2>
   <button @click="add()">add</button>
   <button @click="rev()">rev</button>
