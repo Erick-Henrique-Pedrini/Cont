e44eefdf888fd240881cde4999e4c18be6248308
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2025-03-25 08:20:10 -0300

    First-Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+const cont = ref(0)
+function add(){
+  cont.value++
+}
+function rev(){
+  cont.value--
+}
+</script>
+<template>
+  <h1>contador</h1>
+  <h2>{{ cont }}</h2>
+  <button @click="add()">add</button>
+  <button @click="rev()">rev</button>
+
+</template>
